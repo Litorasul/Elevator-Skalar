15bb918e59681e3deeed97d23e91cb75975ab94d
Nikolay Kelov <n_kelov@yahoo.com>
2021-01-12 09:46:05 +0100

    Add Intermediate Stops Functionality and some styling on the buttons

--- scripts.js
+++ scripts.js
@@ -9,8 +9,14 @@
 
     let currentFloor = 4;
     const elevatorStopsQueue = [];
+    const elevatorState = {
+        inMotion: false,
+        direction: '',
+        from: '',
+        to: ''
+    };
     let idleTimeout;
-    let inMotion = false;
+    let flagFloor = '';
 
     currentFloorEl.textContent = currentFloor;
     doorsActionEl.textContent = 'Open';
@@ -28,11 +34,17 @@
         stopIdle();
         if (element.target.nodeName === 'I') {
             const toFloor = element.target.parentNode.classList[0];
+            const direction = element.target.classList[0];
             element.target.classList.add('green');
-            elevatorStopsQueue.push(toFloor);
-            if (!inMotion) {
-                goToSelectedFloors();
+            if (canHaveIntermediateStop(toFloor, direction)) {
+                flagFloor = toFloor;
+            } else {
+                elevatorStopsQueue.push(toFloor);
+                if (!elevatorState.inMotion) {
+                    goToSelectedFloors();
+                }
             }
+
         }
         startIdle();
     }
@@ -48,25 +60,52 @@
 
     //Regular and Intermediate Stops Functionality
     async function goToSelectedFloors() {
-        inMotion = true;
+        elevatorState.inMotion = true;
         while (elevatorStopsQueue.length !== 0) {
             const to = elevatorStopsQueue.shift();
+            elevatorState.from = currentFloor;
+            elevatorState.to = to;
             await moveElevator(currentFloor, to);
         }
-        inMotion = false;
+        elevatorState.inMotion = false;
     }
 
+    function canHaveIntermediateStop(toFloor, direction) {
+        if (!elevatorState.inMotion) {
+            return false;
+        }
+        if (elevatorState.from === '' && elevatorState.to === '') {
+            return false;
+        }
+        if (elevatorState.direction !== direction && direction !== 'fas') {
+            return false;
+        }
+        if (direction === 'up') {
+            if (toFloor < elevatorState.from || toFloor > elevatorState.to) {
+                return false;
+            }
+        }
+        if (direction === 'down') {
+            if (toFloor > elevatorState.from || toFloor < elevatorState.to) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     // Elevator Engine Functionality
     async function moveElevator(from, to) {
         doorsActionEl.textContent = 'Closed';
         if (from < to) {
             // move up
+            elevatorState.direction = 'up';
             for (let i = from; i <= to; i++) {
                 await nextFloor(i);
             }
         } else if (from > to) {
             // move down
+            elevatorState.direction = 'down';
             for (let i = from; i >= to; i--) {
                 await nextFloor(i);
             }
@@ -81,10 +120,15 @@
         await sleep(1000);
         currentFloor = floor;
         currentFloorEl.textContent = floor;
+        if (flagFloor == floor) {
+            await stopOnFloor(floor);
+            flagFloor = '';
+        }
     };
 
     async function stopOnFloor(floor) {
         doorsActionEl.textContent = 'Open';
+        elevatorState.direction = '';
         const elements = document.getElementsByClassName(floor);
         for (let i = 0; i < elements.length; i++) {
             [...elements[i].children].forEach(element => {
